f7f36709eb2852841c97246f4e569facb7762a00
Maylon <maylongatodemais25@gmail.com>
2023-03-16 11:13:34 -0300

    gtuhtgh

--- lib/models/consulta_produtos_models.dart
+++ lib/models/consulta_produtos_models.dart
@@ -1,3 +1,11 @@
-class ConsultaProdutosModels{
-  
+class ConsultaProdutosModels {
+  String localProduto = '';
+  String nomeProduto = '';
+  String estadoProduto = '';
+  //construtor do modelo
+ConsultaProdutosModels(
+      this.estadoProduto,
+      this.localProduto,
+      this.nomeProduto
+ );
 }
