984ca81bea73910675e4bf8c4a71e00b4bef2a2e
Luis Fabiano <luidibaricheli000@gmail.com>
2023-03-16 09:49:59 -0300

    codigo e botao buscar

--- lib/ui/codigo_estoque.dart
+++ lib/ui/codigo_estoque.dart
@@ -30,41 +30,48 @@ class _CodigoEstoqueState extends State<CodigoEstoque> {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                Padding(padding: EdgeInsets.symmetric(vertical: 25)),
-                Row(
-                  children: [
-                    TextField(
-                      maxLength: 10,
-                      keyboardType: TextInputType.number,
-                      focusNode: codigofocus,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: markPrimaryColor)),
-                          labelText: 'Código',
-                          labelStyle:
-                              TextStyle(color: markPrimaryColor, fontSize: 14,fontWeight: FontWeight.bold),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          )),
-                    ),
-                TextButton(
-                  child: Text(
-                    'Buscar',
-                    style: TextStyle(fontSize: 23, color: Colors.white),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+                child: Container(
+                  height: 60,
+                  width: 200,
+                  child: TextField(
+                    maxLength: 10,
+                    keyboardType: TextInputType.number,
+                    focusNode: codigofocus,
+                    decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: markPrimaryColor)),
+                        labelText: 'Código',
+                        labelStyle: TextStyle(
+                            color: markPrimaryColor,
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        )),
                   ),
-                  style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(markPrimaryColor),
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(13)))),
-                  onPressed: () {},
                 ),
-              ],
-            ),
-              ],
-            ),
+              ),
+              Container(
+                height: 61,
+                width: 160,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: PrimaryButton(
+                      text: 'Buscar',
+                      onPressed: () {
+                        // carregaHoraLogin();
+                        // FocusScope.of(context).unfocus();
+                        // login(emailController.text.trim().toLowerCase(),
+                        //     passwordController.text);
+                        //  AppManager().validLogin = false;
+                      }),
+                ),
+              )
+            ],
           ),
         ),
       ),
